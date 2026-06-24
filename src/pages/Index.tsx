50bfb42aad7838c2eafd89e651a8d5a5e972401f
AldisG <aldisgailis1@gmail.com>
2026-06-25 01:09:51 +0300

    adjustements to wording

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,7 +6,7 @@ const Index = () => {
     <ContentWrapper>
       <div className="text-center space-y-8 max-w-md w-full py-4">
         <div className="space-y-2">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white">LingoFlow</h1>
+          <h1 className="text-5xl font-extrabold tracking-tight text-white">Flash Cards Canvas</h1>
           <p className="text-xl text-slate-300">Practice your wrods.</p>
         </div>
 
